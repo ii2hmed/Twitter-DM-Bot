6418b882d62a7220b0cc32ba0555a2993922bbf0
ii2hmed <57506634+ii2hmed@users.noreply.github.com>
2019-11-08 04:02:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:				'xxxxxxxxxxxxxxxxx',
-    consumer_secret:			'xxxxxxxxxxxxxxxxx',
-    access_token:				'xxxxxxxxxxxxxxxxx',
-    access_token_secret:		'xxxxxxxxxxxxxxxxx'
+    consumer_key:				'HzIKvfnkgGlTcczCOa1OjNhUb',
+    consumer_secret:			'c6KXmsWZ0GK4IPJsv9mIxsGSUXMVBC6Wv9apXNya2jfJswvmUs',
+    access_token:				'3043427035-FKCKsDxUMfWLFTvENmtHHkzkcbazDT8vbzTYaLO',
+    access_token_secret:		'7L0aByW3jzp4qGfA0pYLFeUqbO1JKnOwsBF72WdRyYFf7'
 }
